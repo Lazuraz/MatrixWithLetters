2038d21ab308a79197b363a4a6fd2181fc4e02e2
Brandon Echegaray <brandonechegary@yahoo.com>
2024-05-04 16:09:01 -0500

    making the symbols and making the color and starting a class

--- project.py
+++ project.py
@@ -1,11 +1,42 @@
 import pygame
+import os
 from random import choice
 from random import randrange
 
 pygame.init()
-width = 1920
-Height =  1080
-RES = (width, Height)
+os.environ['SDL_VIDEO_CENTERED'] = '1'#this will center the screen
+resolution = (800, 600)
+
+font_height = 35
+
+Katakana = ['ｦ', 'ｱ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 
+            'ｾ', 'ｿ', 'ﾀ', 'ﾂ', 'ﾃ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ',
+          'ﾊ', 'ﾋ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾗ', 'ﾘ', 'ﾜ', '日']
+font = pygame.font.Font('font/ms mincho.ttf', font_height)
+green_symbols = [font.render(char, True, (0, 255, 0)) for char in Katakana] #will make the katakana green.
+
+screen = pygame.display.set_mode(resolution)
+clock = pygame.time.Clock()
+show_surface = pygame.Surface(resolution)#will use this to show the surface of my resolution.
+
+class Symbol:
+       
+
+running = True
+while running: 
+     show_surface.fill(pygame.Color(0, 0, 0 ))
+     screen.blit(show_surface, (0, 0))
+
+     pygame.display.update()
+
+     clock.tick(60)
+    
+
+
+
+for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
 
 
 
